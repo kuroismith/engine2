68a8d7d8ccad2fb5add185e324ff46ed61a7b186
wuqinchun <kuroismith@gmail.com>
2018-09-27 21:07:30 +0800

    Add ImageMetadata

---
+++ src/main/scala/org/engine2/core/model/ImageMetadata.scala
@@ -0,0 +1,24 @@
+package org.engine2.core.model
+
+import geotrellis.proj4.CRS
+import geotrellis.raster.CellType
+import geotrellis.spark.{Bounds, LayerId, SpatialKey, TileLayerMetadata}
+import geotrellis.spark.tiling.LayoutDefinition
+import geotrellis.vector.Extent
+
+// A Image is a list of tileLayerRDD with ImageMetadata
+case class ImageMetadata[Z: Range](
+  id: String,
+  zoomRange: Range,
+  override val cellType: CellType,
+  override val layout: LayoutDefinition,
+  override val extent: Extent,
+  override val crs: CRS,
+  override val bounds: Bounds[SpatialKey]
+) extends TileLayerMetadata[SpatialKey](cellType, layout, extent, crs, bounds) {
+
+  def layerId(zoom: Int): LayerId = {
+    assert(zoomRange.contains(zoom), s"zoom should be within zoomRange: $zoomRange")
+    LayerId(id, zoom)
+  }
+}
