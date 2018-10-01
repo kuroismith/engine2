a221a90e424a87203f2d76391b3d1edce1c9ab53
wuqinchun <kuroismith@gmail.com>
2018-10-01 16:31:32 +0800

    Refactor ZoomRange::zoomInRange to ZoomRange::contain

--- src/main/scala/org/engine2/core/model/ZoomRange.scala
+++ src/main/scala/org/engine2/core/model/ZoomRange.scala
@@ -3,5 +3,5 @@ package org.engine2.core.model
 case class ZoomRange(minZoom: Int, maxZoom: Int) {
   override def toString: String = s"ZoomRange(minZoom = $minZoom, maxZoom = $maxZoom)"
 
-  def zoomInRange(zoom: Int): Boolean = zoom >= minZoom && zoom <= maxZoom
+  def contain(zoom: Int): Boolean = zoom >= minZoom && zoom <= maxZoom
 }
